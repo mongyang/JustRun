e86c7c81f91c19be05ae12e3e77426fd834afbf9
mongyang <mongyang83@gmail.com>
2017-11-17 14:26:32 +0900

    Add MyProject

---
+++ MyProject/Assets/Scripts/BackGroundScroller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGroundScroller : MonoBehaviour {
+	
+	public float scroll_speed = 1.0f; 
+	private Material target_material;
+	private float temp_offset;
+
+	void Start () {
+		target_material = GetComponent<Renderer>().material; 
+	}
+
+	void Update () {
+
+		temp_offset += Time.deltaTime * scroll_speed;
+		target_material.mainTextureOffset = new Vector2 (temp_offset, 0);
+	}
+}
---
+++ MyProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float jump_power = 5.0f;
+
+	private Rigidbody2D rigid;
+	private Animator animator;
+	private bool jump_state = false;
+	// Use this for initialization
+	void Start () {
+		rigid = gameObject.GetComponent<Rigidbody2D> ();
+		animator = gameObject.GetComponentInChildren<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Jump")) {
+			jump_state = true;
+			animator.SetTrigger ("doing_jump");
+		}
+	}
+
+	void FixedUpdate() {
+		Jump ();
+	}
+
+	void Jump() {
+		if (!jump_state)
+			return;
+
+		rigid.velocity = Vector2.zero;
+
+		Vector2 jump_velocity = new Vector2 (0, jump_power);
+		rigid.AddForce (jump_velocity, ForceMode2D.Impulse);
+
+		jump_state = false;
+	}
+}
